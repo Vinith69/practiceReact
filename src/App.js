bbc7af4bef473d9b3af5224d80a7f0292ec85e71
vinith69 <ramarock09@gmial.com>
2020-11-14 15:00:41 +0530

    My first react practice web

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import AddComponent from "./Components/AddComponent";
+
+function App() {
+	return (
+		<div className="main-page">
+			<p>Hello, Welcome to practice page Maths</p>
+			<AddComponent />
+		</div>
+	);
+}
+
+export default App;
